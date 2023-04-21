883e3ea5986b445bd4a166cc0b8ec89fd5f7bb16
ishanuc <ishanu.chattopadhyay@gmail.com>
2023-04-21 13:55:50 -0500

    upd

--- tex/grant/getparts.sh
+++ tex/grant/getparts.sh
@@ -2,5 +2,20 @@
 
 FILE=$1
 
-pdfjam $FILE 1 --letterpaper -o summary.pdf
-pdfjam $FILE 2- --letterpaper -o narrative.pdf
+pdfjam $FILE 1-5 --letterpaper -o narrative.pdf
+pdfjam $FILE 6 --letterpaper -o abbreviations.pdf
+pdfjam $FILE 7-8 --letterpaper -o datamanagement.pdf
+pdfjam $FILE 9 --letterpaper -o technicalabstract.pdf
+pdfjam $FILE 10 --letterpaper -o layabstract.pdf
+pdfjam $FILE 11-13 --letterpaper -o sow.pdf
+pdfjam $FILE 14 --letterpaper -o impact.pdf
+pdfjam $FILE 15 --letterpaper -o militaryhealth.pdf
+pdfjam $FILE 16-18 --letterpaper -o facilities.pdf
+pdfjam $FILE 19 --letterpaper -o patentspubs.pdf
+pdfjam $FILE 22-23 --letterpaper -o IP.pdf
+pdfjam $FILE 24 --letterpaper -o datasharing.pdf
+pdfjam $FILE 25- --letterpaper -o refrences.pdf
+
+
+
+
---
+++ tex/notes.md
@@ -0,0 +1,11 @@
+refcomparison: minimized the earth
+mover’s distance between HA amino acid sequences
+from the observed population one year in the
+future and the estimated
+population produced by the fitness model
+
+H3N2 HA
+2017-18 3 vs 8
+2018-19 6  vs 12
+
+
