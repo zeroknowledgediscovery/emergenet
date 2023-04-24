5fff8bf6259cefb72a97044517faebad3230d07b
ishanuc <ishanu.chattopadhyay@gmail.com>
2023-04-24 07:23:51 -0500

    upd

--- tex/grant/getparts.sh
+++ tex/grant/getparts.sh
@@ -10,11 +10,11 @@ pdfjam $FILE 10 --letterpaper -o layabstract.pdf
 pdfjam $FILE 11-13 --letterpaper -o sow.pdf
 pdfjam $FILE 14 --letterpaper -o impact.pdf
 pdfjam $FILE 15 --letterpaper -o militaryhealth.pdf
-pdfjam $FILE 16-18 --letterpaper -o facilities.pdf
-pdfjam $FILE 19 --letterpaper -o patentspubs.pdf
-pdfjam $FILE 22-23 --letterpaper -o IP.pdf
-pdfjam $FILE 24 --letterpaper -o datasharing.pdf
-pdfjam $FILE 25- --letterpaper -o refrences.pdf
+pdfjam $FILE 16-20 --letterpaper -o facilities.pdf
+pdfjam $FILE 21 --letterpaper -o patentspubs.pdf
+pdfjam $FILE 24-25 --letterpaper -o IP.pdf
+pdfjam $FILE 26 --letterpaper -o datasharing.pdf
+pdfjam $FILE 27- --letterpaper -o refrences.pdf
 
 
 
