655bb67c862cc7c6c9521a10d1b91d4c15898ed0
ishanuc <ishanu.chattopadhyay@gmail.com>
2023-11-15 18:38:02 -0600

    updated

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
         "numpy",  
         "pandas",
         "joblib",
-        "quasinet==0.1.50",
+        "quasinet==0.1.51",
         "scipy"],
     python_requires='>=3.6',
     classifiers=[
